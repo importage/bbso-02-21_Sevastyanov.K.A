405010aa36f9934fc81205f5ccdb4fb8a9e72d29
importage <sevastyanov.cs@gmail.com>
2023-01-12 04:32:46 +0300

    cmd_casino v 0.9 main dont work, conflict_err 409(database_added)

--- main.py
+++ main.py
@@ -3,12 +3,26 @@ import telebot
 from telebot import types
 import time
 from threading import Thread
+import sqlite3
 
 
 bot = telebot.TeleBot('5807437640:AAEO4V6Nva895NVkw7xdIejK2WV9BucSUJw')
+conn = sqlite3.connect('db/test_db.db', check_same_thread=False)
+cursor = conn.cursor()
+
+
+def db_table_val(user_id: int, user_name: str, user_surname: str, username: str):
+    cursor.execute('INSERT INTO test_db (user_id, user_name, user_surname, username) VALUES (?, ?, ?, ?)', (user_id, user_name, user_surname, username))
+    conn.commit()
 @bot.message_handler(content_types=['text'])
 def start_main(message):
     if message.text == '/start':
+        us_id = message.from_user.id
+        us_name = message.from_user.first_name
+        us_sname = message.from_user.last_name
+        username = message.from_user.username
+        db_table_val(user_id=us_id, user_name=us_name, user_surname=us_sname, username=username)
+
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         item_bj = types.KeyboardButton('Черный Джек')
         item_roulette = types.KeyboardButton('Рулетка')
